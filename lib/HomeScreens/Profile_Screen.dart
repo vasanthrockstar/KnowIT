623706656a8ce1109eaaf34e1ac359f0690d5786
vasanth <v.g.vasanthakumar@gmail.com>
2020-03-25 00:43:03 +0530

    profile page

--- lib/HomeScreens/Profile_Screen.dart
+++ lib/HomeScreens/Profile_Screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:know_it_master/Database_models/UserDetails.dart';
 import 'package:know_it_master/HomeScreens/Profile_Tabs/my_links.dart';
@@ -69,143 +68,140 @@ class _F_ProfilePageState extends State<F_ProfilePage> {
         stream: widget.database.readUser(USER_ID),
         builder: (context, snapshot) {
           final user = snapshot.data;
-        return new MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: new DefaultTabController(
-            length: categories.length,
-            child: new Scaffold(
-              appBar: PreferredSize(
-                preferredSize: Size.fromHeight(460),
-                //preferredSize : Size(double.infinity, 100),
-                child: CustomAppBar(
-                  leftActionBar: Container(
-                    child: Icon(
-                      Icons.arrow_back,
-                      size: 40,
-                      color: Colors.black38,
-                    ),
-                  ),
-                  leftAction: () {
-                    Navigator.pop(context, true);
-                  },
-                  rightActionBar: Container(
-                    child:IconButton(
-                      icon: Icon(
-                        Icons.settings,
+          return new MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: new DefaultTabController(
+              length: categories.length,
+              child: new Scaffold(
+                appBar: PreferredSize(
+                  preferredSize: Size.fromHeight(400),
+                  //preferredSize : Size(double.infinity, 100),
+                  child: CustomAppBar(
+                    leftActionBar: Container(
+                      child: Icon(
+                        Icons.arrow_back,
                         size: 40,
-                        color: backgroundColor,
+                        color: Colors.black38,
                       ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SettingsPage() ),
-                          );
-                        }
                     ),
-                  ),
-                  rightAction: () {
-                    print('right action bar is pressed in appbar');
-                  },
-                  primaryText: null,
-                  secondaryText: 'Profile',
-                  profile: Container(
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Center(
-                        child: Column(
-                          children: <Widget>[
-                            Positioned(
-                              child: (user != null ? user.userImagePath : "") != null ? CircleAvatar(
-                                radius: 40.0,
-                                backgroundColor: Colors.grey[200],
-                                backgroundImage:
-                                NetworkImage((user != null ? user.userImagePath : "")),
-                              ) : CircleAvatar(
-                                radius: 30.0,
-                                backgroundColor: Colors.grey[200],
-                              ),
-                            ),
-
-                            SizedBox(height: 10,),
-                            Text(user != null ? user.phoneNumber : 'loading...',style: descriptionStyleDarkBlur,),
-                            SizedBox(height: 10,),
-                            Text(user != null ? user.username : 'loading...',style: subTitleStyle,),
-                            SizedBox(height: 10,),
-                            Divider(
-                              color: Colors.black.withOpacity(0.5),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                SizedBox(width: 10,),
-                                Column(
-                                  children: <Widget>[
-                                    Text("Links",style: descriptionStyleDark),
-                                    Text("01",style: subTitleStyleLight,)
-                                  ],
+                    leftAction: () {
+                      Navigator.pop(context, true);
+                    },
+                    rightActionBar: Container(
+                      child:IconButton(
+                          icon: Icon(
+                            Icons.settings,
+                            size: 40,
+                            color: backgroundColor,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SettingsPage() ),
+                            );
+                          }
+                      ),
+                    ),
+                    rightAction: () {
+                      print('right action bar is pressed in appbar');
+                    },
+                    primaryText: null,
+                    secondaryText: 'Profile',
+                    profile: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Center(
+                          child: Column(
+                            children: <Widget>[
+                              Center(
+                                child: (user != null ? user.userImagePath : "") != null ? CircleAvatar(
+                                  radius: 40.0,
+                                  backgroundColor: Colors.grey[200],
+                                  backgroundImage:
+                                  NetworkImage((user != null ? user.userImagePath : "")),
+                                ) : CircleAvatar(
+                                  radius: 30.0,
+                                  backgroundColor: Colors.grey[200],
                                 ),
-                                Column(children: <Widget>[
-                                  Text("Media",style: descriptionStyleDark),
-                                  Text("02",style: subTitleStyleLight,)
-                                ],),
-                                Column(children: <Widget>[
-                                  Text("Reacted",style: descriptionStyleDark),
-                                  Text("03",style: subTitleStyleLight,)
-                                ],),
-                                SizedBox(width: 10,),
-                              ],
-                            ),
-                            Divider(
-                              color: Colors.black.withOpacity(0.5),
-                            ),
-                            SizedBox(height: 10,),
-                          ],
+                              ),
+
+                              SizedBox(height: 10,),
+                              Text(user != null ? user.phoneNumber : 'loading...',style: descriptionStyleDarkBlur,),
+                              SizedBox(height: 10,),
+                              Text(user != null ? user.username : 'loading...',style: subTitleStyle,),
+                              SizedBox(height: 10,),
+                              Divider(
+                                color: Colors.black.withOpacity(0.5),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  SizedBox(width: 10,),
+                                  Column(
+                                    children: <Widget>[
+                                      Text("Links",style: descriptionStyleDark),
+                                      Text("01",style: subTitleStyleLight,)
+                                    ],
+                                  ),
+                                  Column(children: <Widget>[
+                                    Text("Media",style: descriptionStyleDark),
+                                    Text("02",style: subTitleStyleLight,)
+                                  ],),
+                                  Column(children: <Widget>[
+                                    Text("Reacted",style: descriptionStyleDark),
+                                    Text("03",style: subTitleStyleLight,)
+                                  ],),
+                                  SizedBox(width: 10,),
+                                ],
+                              ),
+                              Divider(
+                                color: Colors.black.withOpacity(0.5),
+                              ),
+                              TabBar(
+                                labelColor: backgroundColor,
+                                labelStyle: selectedLabel,
+                                unselectedLabelStyle: unselectedLabel,
+                                isScrollable: true,
+                                tabs: categories.map((Category choice) {
+                                  return new Tab(
+                                    text: choice.name,
+                                  );
+                                }).toList(),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
-                  tabBarWidget: Center(
-                    child: new TabBar(
-                      labelColor: backgroundColor,
-                      labelStyle: selectedLabel,
-                      unselectedLabelStyle: unselectedLabel,
-                      isScrollable: true,
-                      tabs: categories.map((Category choice) {
-                        return new Tab(
-                          text: choice.name,
-                        );
-                      }).toList(),
-                    ),
-                  ),
+                ),
+
+                body: new TabBarView(
+                  children: categories.map((Category choice) {
+                    if (choice.name == 'My Links') {
+                      return new Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: new MyLinksPage(choice: choice, database: widget.database,),
+                      );
+                    } else if (choice.name == 'My Media') {
+                      return new Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: new MyMediaPage(choice: choice, database: widget.database,),
+                      );
+                    }
+                    else{
+                      return new Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: new MyReactionsPage(choice: choice),
+                      );
+                    }
+                  }).toList(),
                 ),
               ),
-
-              body: new TabBarView(
-                children: categories.map((Category choice) {
-                  if (choice.name == 'My Links') {
-                    return new Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: new MyLinksPage(choice: choice, database: widget.database,),
-                    );
-                  } else if (choice.name == 'My Media') {
-                    return new Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: new MyMediaPage(choice: choice, database: widget.database,),
-                    );
-                  }
-                  else{
-                    return new Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: new MyReactionsPage(choice: choice),
-                    );
-                  }
-                }).toList(),
-              ),
             ),
-          ),
-        );
-      }
+          );
+        }
     );
   }
 }
